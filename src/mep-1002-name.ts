fc8b856744455023a5e7ea359a7a73876b0c1636
Mr.Mao <951416545@qq.com>
2024-04-01 20:50:16 +0800

    chore: update change

--- src/mep-1002-name.ts
+++ src/mep-1002-name.ts
@@ -4,7 +4,7 @@ import {
 } from "../generated/MEP1002Name/MEP1002Name"
 import {NftAsset } from "../generated/schema"
 
-const Mep1002Address = '0xad5a1855a383732f311241c1a4f9510da0ad0743'
+const Mep1002Address = '0x1964F08f56b79051fB3AE9a2C4d8D92A059b1237'
 
 export function handleTransfer(event: TransferEvent): void {
   let key = `${Mep1002Address}-${event.params.tokenId}`
